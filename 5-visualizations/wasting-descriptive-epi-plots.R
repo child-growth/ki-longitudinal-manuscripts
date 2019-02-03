a9541156f425a6c468acf355fc46addfc688a852
sonaliii <sona8242@gmail.com>
2019-02-02 18:26:44 -0800

    switch to error bar and fix studies/children labels

--- 5-visualizations/wasting-descriptive-epi-plots.R
+++ 5-visualizations/wasting-descriptive-epi-plots.R
@@ -26,7 +26,7 @@ levels(d$region)[asia_region] = 'South Asia'
 #-------------------------------------------------------------------------------------------
 ki_desc_plot <- function(d, Disease, Measure, Birth, Severe, Age_range, 
                         Cohort="pooled",
-                        xlabel="Age category",
+                        xlabel="Age (months)",
                         ylabel="",
                         h1=0,
                         h2=3){
@@ -45,9 +45,14 @@ ki_desc_plot <- function(d, Disease, Measure, Birth, Severe, Age_range,
   df <- df %>% mutate(nmeas.f = gsub('N=', '', nmeas.f)) %>%
     mutate(nstudy.f = gsub('N=', '', nstudy.f))
   
+  # remove text from labels
+  df <- df %>% mutate(nmeas.f = gsub(' children', '', nmeas.f)) %>%
+    mutate(nstudy.f = gsub(' studies', '', nstudy.f))
+  
   p <- ggplot(df,aes(y=est,x=agecat)) +
-    geom_point(aes(fill=region, color=region), size = 4) +
-    geom_linerange(aes(ymin=lb, ymax=1.3*ub, color=region),  alpha=0.5, size = 3) +
+    geom_errorbar(aes(color=region, ymin=lb, ymax=ub)) +
+    # geom_point(aes(fill=region, color=region), size = 4) +
+    # geom_linerange(aes(ymin=lb, ymax=ub, color=region),  alpha=0.5, size = 3) +
     scale_color_manual(values=tableau11, drop=TRUE, limits = levels(df$measure)) +
     xlab(xlabel)+
     ylab(ylabel) +
@@ -55,12 +60,17 @@ ki_desc_plot <- function(d, Disease, Measure, Birth, Severe, Age_range,
                            label = nmeas.f), size = 4) +
     geom_text(data=df, aes(x = agecat, y = h1, vjust = -1, 
                            label = nstudy.f), size = 4) +
-    scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) + 
+    scale_x_discrete(expand = expand_scale(add = 2)) +
+    annotate('text', x = -0.25, y = h1, label = 'Studies:', vjust = -1) +
+    annotate('text', x = -0.25, y = h1, label = 'Children:', vjust = 1) +
+    scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
+    expand_limits(y = h2) +
+    # annotate("blank", x = 0, y = h1) +
     theme(axis.text.x = element_text(margin = 
-                                       margin(t = -30, r = 0, b = 0, l = 0),
+                                       margin(t = -40, r = 0, b = 0, l = 0),
                                      size = 15)) +
     theme(axis.title.x = element_text(margin = 
-                                        margin(t = 25, r = 0, b = 0, l = 0))) +
+                                        margin(t = 30, r = 0, b = 0, l = 0))) +
     # annotate("text",label=df$ptest.f,x=df$agecat,
     #          y=df$est,hjust=-2,size=3)+
     ggtitle("") +
@@ -127,7 +137,7 @@ p1 <- ki_desc_plot(d,
                    Severe="no", 
                    Age_range="3 months", 
                    Cohort="pooled",
-                   xlabel="Age category",
+                   xlabel="Age in months",
                    h1=25,
                    h2=28)
 
@@ -146,7 +156,7 @@ p2 <- ki_desc_plot(d,
                    Severe="no", 
                    Age_range="3 months", 
                    Cohort="pooled",
-                   xlabel="Age category",
+                   xlabel="Age in months",
                    h1=36,
                    h2=42)
 
@@ -164,7 +174,7 @@ p3 <- ki_desc_plot(d,
                    Severe="no", 
                    Age_range="6 months", 
                    Cohort="pooled",
-                   xlabel="Age category",
+                   xlabel="Age in months",
                    h1=4.5,
                    h2=5.25)
 
@@ -183,9 +193,9 @@ p4 <- ki_desc_plot(d,
                    Severe="no", 
                    Age_range="90 days", 
                    Cohort="pooled",
-                   xlabel="Age category",
-                   h1=120,
-                   h2=10)
+                   xlabel="Age in months",
+                   h1=105,
+                   h2=110)
 
 
 ggsave(p4, file="figures/wasting/pooled_rev.png", width=10, height=8)
@@ -203,9 +213,9 @@ p5 <- ki_desc_plot(d,
                    Severe="no", 
                    Age_range="6 months", 
                    Cohort="pooled",
-                   xlabel="Age category",
-                   h1=22.5,
-                   h2=5.25)
+                   xlabel="Age in months",
+                   h1=20,
+                   h2=22)
 
 
 ggsave(p5, file="figures/wasting/pooled_pers.png", width=10, height=8)
