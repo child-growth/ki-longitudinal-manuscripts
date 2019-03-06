37ab63c1a8b0fd405c9343a9340064b306f85e01
Jade Benjamin-Chung <jadebc@gmail.com>
2019-03-05 17:15:21 -0800

    add bar plot

--- 5-visualizations/stunting-flow.R
+++ 5-visualizations/stunting-flow.R
@@ -5,10 +5,10 @@ rm(list=ls())
 source(paste0(here::here(), "/0-config.R"))
 
 # load fake data
-# stunt_data = readRDS(paste0(res_dir, "fakeflow.RDS"))
+stunt_data = readRDS(paste0(res_dir, "fakeflow.RDS"))
 
 # load real data
-stunt_data = readRDS(paste0(res_dir, "stuntflow.RDS"))
+# stunt_data = readRDS(paste0(res_dir, "stuntflow.RDS"))
 
 plot_data = stunt_data %>%
   mutate(classif = case_when(
@@ -27,6 +27,9 @@ plot_data = stunt_data %>%
 
 mycols = c("#11466B", tableau10[1], tableau10[4], "#811818")
 
+#-----------------------------------------
+# Alluvial flow plot
+#-----------------------------------------
 flow_plot = ggplot(plot_data,
        aes(x = agecat,  
            alluvium = subjid,
@@ -42,6 +45,30 @@ flow_plot = ggplot(plot_data,
 
 #ggsave(flow_plot, file="figures/stunting/pool_flow_fake.png", width=10, height=5)
 ggsave(flow_plot, file="figures/stunting/pool_flow.png", width=10, height=5)
+
+#-----------------------------------------
+# bar graphs without alluvival flow between each child
+#-----------------------------------------
+age_classif_totals = plot_data %>%
+  group_by(agecat, classif) %>%
+  summarise(n = sum(freq))
+
+age_totals = plot_data %>%
+  group_by(agecat) %>%
+  summarise(tot = sum(freq))
+
+bar_plot_data = full_join(age_classif_totals, age_totals, by = c("agecat"))
+
+bar_plot_data = bar_plot_data %>% mutate(percent = n/tot * 100)
+
+ggplot(bar_plot_data) +
+  geom_bar(aes(x = agecat, y = percent, fill = classif), stat="identity", width=0.5) +
+  scale_fill_manual("", values = mycols) +
+  theme(legend.position = "bottom") +
+  xlab("Child age") + ylab("Percentage of children")
+
+
+
 
 
 
