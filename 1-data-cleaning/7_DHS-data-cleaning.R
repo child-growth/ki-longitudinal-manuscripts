a5208dd097c06a5abe86e873f5a29607f316a61f
Jade Benjamin-Chung <jadebc@gmail.com>
2019-03-11 16:57:06 -0700

    Jade's comments

--- 1-data-cleaning/7_DHS-data-cleaning.R
+++ 1-data-cleaning/7_DHS-data-cleaning.R
@@ -10,29 +10,54 @@ source(paste0(here::here(), "/0-config.R"))
 
 
 df <- read_rds(paste0(data_dir, "dhs-combined/", "dhs_data_combined.rds"))
-df1 <- read_rds(paste0(data_dir, "AOIR71DT/AOIR71FL.DTA"))
+# df1 <- read_rds(paste0(data_dir, "AOIR71DT/AOIR71FL.DTA"))
 
 #-------------------------------------------
 # Drop unnecessary variables and rename selected variables
 #-------------------------------------------
 
-d <- df %>% rename(country = v000, 
-                   year = v007)
+df <- df %>% rename(country = v000, 
+                   year = v007) %>%
+  # Wendy please confirm it is ok to drop this: 
+  select(-s308b8)
 
-d[, c(4:23,56:59)] <- NULL
+# Wendy, I suggest you change this to use
+# column names instead of numbers. If you reran
+# the data processing and the row or column numbers
+# changed, this would no longer be reproducible. 
+# d[, c(4:23,56:59)] <- NULL
+
+# Here is my suggested way of selecting relevant
+# variables: 
+# Wendy: please double check the age variables -- what
+# are the variables that start with b19? 
+age_vars = colnames(df)[grep("b8", colnames(df))]
+haz_vars = colnames(df)[grep("hw70", colnames(df))]
+whz_vars = colnames(df)[grep("hw72", colnames(df))]
+
+d_haz_wide = df %>% select(caseid, country, dataset, year, 
+                  age_vars, haz_vars)
+
+d_whz_wide = df %>% select(caseid, country, dataset, year, 
+                  age_vars, whz_vars)
 
 #-------------------------------------------
 # Reshape from wide to long 
 # Keep one column for case id 
 # Convert age in months, WHZ, HAZ from wide to long 
 #-------------------------------------------
-d$hw70_7 <- d$hw70_8 <- d$hw70_9 <- d$hw70_10 <- d$hw70_11 <- d$hw70_12 <- d$hw70_13 <- d$hw70_14 <- d$hw70_15 <- d$hw70_16 <- d$hw70_17 <- d$hw70_18 <- d$hw70_19 <- d$hw70_20 <- NA
 
-d$hw72_7 <- d$hw72_8 <- d$hw72_9 <- d$hw72_10 <- d$hw72_11 <- d$hw72_12 <- d$hw72_13 <- d$hw72_14 <- d$hw72_15 <- d$hw72_16 <- d$hw72_17 <- d$hw72_18 <- d$hw72_19 <- d$hw72_20 <- NA
+# Wendy: an easier to read way of doing this is as follows
+# I'm also not sure why you're assigning all of these to NAs
 
-d = d[, c(1:29, 36, 39, 38, 37, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 30:35, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50)]
+# d$hw70_7 <- d$hw70_8 <- d$hw70_9 <- d$hw70_10 <- d$hw70_11 <- d$hw70_12 <- d$hw70_13 <- d$hw70_14 <- d$hw70_15 <- d$hw70_16 <- d$hw70_17 <- d$hw70_18 <- d$hw70_19 <- d$hw70_20 <- NA
 
-d.long <- reshape(d, varying=list(c(4:23), c(24:43), c(44:63)), direction="long", idvar="caseid", sep="_")
+# d$hw72_7 <- d$hw72_8 <- d$hw72_9 <- d$hw72_10 <- d$hw72_11 <- d$hw72_12 <- d$hw72_13 <- d$hw72_14 <- d$hw72_15 <- d$hw72_16 <- d$hw72_17 <- d$hw72_18 <- d$hw72_19 <- d$hw72_20 <- NA
+
+# same comment -- hard coding is not as reproducible
+# d = d[, c(1:29, 36, 39, 38, 37, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 30:35, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50)]
+
+# d.long <- reshape(d, varying=list(c(4:23), c(24:43), c(44:63)), direction="long", idvar="caseid", sep="_")
 
 
 # Age columns start with "b". Data for to 20 children were recorded per woman. 
