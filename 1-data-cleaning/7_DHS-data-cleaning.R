9e7e3b4467cfa8ee050003ab321809eaf2d1dc29
Wendy Jilek <wmjilek@berkeley.edu>
2019-03-12 16:24:33 -0700

    updated script

--- 1-data-cleaning/7_DHS-data-cleaning.R
+++ 1-data-cleaning/7_DHS-data-cleaning.R
@@ -16,16 +16,12 @@ df <- read_rds(paste0(data_dir, "dhs-combined/", "dhs_data_combined.rds"))
 # Drop unnecessary variables and rename selected variables
 #-------------------------------------------
 
-df <- df %>% rename(country = v000, 
+d <- df %>% rename(country = v000, 
                    year = v007) %>%
-  # Wendy please confirm it is ok to drop this: 
-  select(-s308b8)
+  select("caseid", "country", "year", "dataset", grep("hw", colnames(df))) %>%
+  select(-contains("sh"))
 
-# Wendy, I suggest you change this to use
-# column names instead of numbers. If you reran
-# the data processing and the row or column numbers
-# changed, this would no longer be reproducible. 
-# d[, c(4:23,56:59)] <- NULL
+
 
 # Here is my suggested way of selecting relevant
 # variables: 
@@ -46,19 +42,16 @@ d_whz_wide = df %>% select(caseid, country, dataset, year,
 # Keep one column for case id 
 # Convert age in months, WHZ, HAZ from wide to long 
 #-------------------------------------------
+d_haz_wide <- d %>%
+  select("caseid", "country", "year", "dataset", "hw1_1", "hw1_2", "hw1_3", "hw1_4", "hw1_5", "hw1_6", "hw70_1", "hw70_2", "hw70_3", "hw70_4", "hw70_5", "hw70_6")
 
-# Wendy: an easier to read way of doing this is as follows
-# I'm also not sure why you're assigning all of these to NAs
+d_haz_long <- reshape(d_haz_wide, varying=list(c("hw1_1", "hw1_2", "hw1_3", "hw1_4", "hw1_5", "hw1_6"), c("hw70_1", "hw70_2", "hw70_3", "hw70_4", "hw70_5", "hw70_6")), direction="long", idvar=c("caseid","country", "year", "dataset"), sep="_")
 
-# d$hw70_7 <- d$hw70_8 <- d$hw70_9 <- d$hw70_10 <- d$hw70_11 <- d$hw70_12 <- d$hw70_13 <- d$hw70_14 <- d$hw70_15 <- d$hw70_16 <- d$hw70_17 <- d$hw70_18 <- d$hw70_19 <- d$hw70_20 <- NA
 
-# d$hw72_7 <- d$hw72_8 <- d$hw72_9 <- d$hw72_10 <- d$hw72_11 <- d$hw72_12 <- d$hw72_13 <- d$hw72_14 <- d$hw72_15 <- d$hw72_16 <- d$hw72_17 <- d$hw72_18 <- d$hw72_19 <- d$hw72_20 <- NA
+d_whz_wide <- d %>%
+  select("caseid", "country", "year", "dataset", "hw1_1", "hw1_2", "hw1_3", "hw1_4", "hw1_5", "hw1_6", "hw72_1", "hw72_2", "hw72_3", "hw72_4", "hw72_5", "hw72_6")
 
-# same comment -- hard coding is not as reproducible
-# d = d[, c(1:29, 36, 39, 38, 37, 49, 48, 47, 46, 45, 44, 43, 42, 41, 40, 30:35, 63, 62, 61, 60, 59, 58, 57, 56, 55, 54, 53, 52, 51, 50)]
-
-# d.long <- reshape(d, varying=list(c(4:23), c(24:43), c(44:63)), direction="long", idvar="caseid", sep="_")
-
+d_qhz_long <- reshape(d_haz_wide, varying=c(4:16), direction="long", idvar="caseid", sep="_", timevar="order")
 
 # Age columns start with "b". Data for to 20 children were recorded per woman. 
 # height for age columns start with "hw70". Data for up to 6 children under age 5 were collected.
