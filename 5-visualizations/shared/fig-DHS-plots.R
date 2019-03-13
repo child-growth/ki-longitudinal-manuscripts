e6ee5cff21986826ccb6062c06912acebcfd7b23
Wendy Jilek <wmjilek@berkeley.edu>
2019-03-13 16:12:31 -0700

    updated DHS plot script

--- 5-visualizations/shared/fig-DHS-plots.R
+++ 5-visualizations/shared/fig-DHS-plots.R
@@ -33,23 +33,122 @@ country.whz <- whz %>% group_by(country) %>% summarize(whz_mn = mean(whz), whz_s
 haz <- merge(haz, country.haz, by="country")
 whz <- merge(whz, country.whz, by="country")
 
-#plots
-p <- ggplot(haz,aes(y=haz_mn,x=agem, group=region)) +
-  stat_smooth(aes(fill=region, color=region), se=F, span = 0.5) +
+
+
+#merge in GHAP data
+load(paste0(here::here(),"/results/desc_data_cleaned.Rdata"))
+
+d = d %>% 
+  mutate(region = ifelse(region=="Asia", "South Asia", region)) %>%
+  mutate(region = factor(region, levels = c("Overall", "Africa", "Latin America", "South Asia")))
+
+
+############################# HAZ plots ###############################
+df <- d %>% filter(
+  disease == "Stunting" &
+    measure == "Mean LAZ" &
+    birth == "yes" &
+    severe == "no" &
+    age_range == "1 month" &
+    cohort == "pooled" 
+)
+
+df <- df %>% filter(region != "Overall")
+
+df <- droplevels(df)
+
+df <- df %>% 
+  arrange(agecat) %>%
+  filter(!is.na(agecat)) %>%
+  filter(!is.na(region)) %>%
+  mutate(agecat = as.character(agecat)) %>%
+  mutate(agecat = ifelse(agecat == "Two weeks", "0", agecat)) %>%
+  mutate(agecat = gsub(" month", "", agecat)) %>%
+  mutate(agecat = gsub(" months", "", agecat)) %>%
+  mutate(agecat = gsub("s", "", agecat)) %>%
+  mutate(agecat = ifelse(agecat == "One", "1", agecat)) %>%
+  mutate(agecat = as.numeric(agecat)) 
+
+names(df)[names(df)=="agecat"] <- "agem"
+
+df <- df %>% mutate(region=case_when(region=="Africa" ~ "AFRO",
+                                     region=="Latin America" ~ "PAHO",
+                                     region=="South Asia" ~ "SEARO"))
+
+#merge together DHS and GHAP data
+combine.haz <- merge(haz, df, by=c("agem","region"))
+
+p <- ggplot(combine.haz,aes(x=agem, group=region)) +
+  stat_smooth(aes(y=haz_mn,fill=region, color=region), se=F, span = 0.5) +
+  stat_smooth(aes(y=est,fill=region, color=region), linetype="dotted", se=F, span = 0.5) +
+  facet_wrap(~region)+
   geom_hline(yintercept = 0, colour = "black") +
   scale_x_continuous(limits = c(0,24), breaks = seq(0,24,2), labels = seq(0,24,2)) + 
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) + 
+  scale_fill_manual(values=tableau11, drop=TRUE, limits = levels(df$measure), 
+                    name = 'Region') +
+  scale_color_manual(values=tableau11, drop=TRUE, limits = levels(df$measure), 
+                     name = 'Region') +
   xlab("Child age, months")+
-  ylab("Mean height-for-age Z-score") +
+  ylab("mean Length-for-age Z-score") +
   ggtitle("") +
-  theme(legend.position="right")
+  theme(strip.text = element_text(margin=margin(t=5))) +
+  theme(legend.position="none")
 
-q <- ggplot(whz,aes(y=whz_mn,x=agem, group=region)) +
-  stat_smooth(aes(fill=region, color=region), se=F, span = 0.5) +
+ggsave(p, file="figures/stunting/fig_stunt_mean_LAZ_region_DHS.png", width=10, height=8)
+
+############################ WHZ plots ###################################
+df <- d %>% filter(
+  disease == "Wasting" &
+    measure == "Mean WLZ" &
+    birth == "yes" &
+    severe == "no" &
+    age_range == "1 month" &
+    cohort == "pooled" 
+)
+
+df <- df %>% filter(region != "Overall")
+
+df <- droplevels(df)
+
+df <- df %>% 
+  arrange(agecat) %>%
+  filter(!is.na(agecat)) %>%
+  filter(!is.na(region)) %>%
+  mutate(agecat = as.character(agecat)) %>%
+  mutate(agecat = ifelse(agecat == "Two weeks", "0", agecat)) %>%
+  mutate(agecat = gsub(" month", "", agecat)) %>%
+  mutate(agecat = gsub(" months", "", agecat)) %>%
+  mutate(agecat = gsub("s", "", agecat)) %>%
+  mutate(agecat = ifelse(agecat == "One", "1", agecat)) %>%
+  mutate(agecat = as.numeric(agecat)) 
+
+names(df)[names(df)=="agecat"] <- "agem"
+
+df <- df %>% mutate(region=case_when(region=="Africa" ~ "AFRO",
+                                     region=="Latin America" ~ "PAHO",
+                                     region=="South Asia" ~ "SEARO"))
+
+#merge together DHS and GHAP data
+combine.whz <- merge(whz, df, by=c("agem","region"))
+
+
+q <- ggplot(combine.whz,aes(x=agem, group=region)) +
+  stat_smooth(aes(y=whz_mn,fill=region, color=region), se=F, span = 0.5) +
+  stat_smooth(aes(y=est,fill=region, color=region), linetype="dotted", se=F, span = 0.5) +
+  facet_wrap(~region)+
   geom_hline(yintercept = 0, colour = "black") +
   scale_x_continuous(limits = c(0,24), breaks = seq(0,24,2), labels = seq(0,24,2)) + 
+  scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) + 
+  scale_fill_manual(values=tableau11, drop=TRUE, limits = levels(df$measure), 
+                    name = 'Region') +
+  scale_color_manual(values=tableau11, drop=TRUE, limits = levels(df$measure), 
+                     name = 'Region') +
   xlab("Child age, months")+
-  ylab("Mean weight-for-height Z-score") +
+  ylab("mean Length-for-age Z-score") +
   ggtitle("") +
-  theme(legend.position="right")
+  theme(strip.text = element_text(margin=margin(t=5))) +
+  theme(legend.position="none")
 
+ggsave(q, file="figures/wasting/fig_wasting_mean_WLZ_region_DHS.png", width=10, height=8)
 
