58d4f69ce089ae7f9bd18edcd6ece5b29bd0916a
PROD\jade.benjamin-chung <jadebc@berkeley.edu>
2019-03-15 22:37:34 +0000

    add age cat fn

--- 2-descriptive-outcomes/stunting/4_stunt_flowdata.R
+++ 2-descriptive-outcomes/stunting/4_stunt_flowdata.R
@@ -12,9 +12,10 @@
 rm(list=ls())
 source(paste0(here::here(), "/0-config.R"))
 
-
+# IS THIS THE RIGHT DATA? 
 load("U:/ucb-superlearner/data/stunting_data.RData")
 
+# load("U:/Data/Stunting/stunting_data.RData")
 
 d = d %>% ungroup() %>% mutate(studyid = as.character(studyid))
 
@@ -43,36 +44,8 @@ d = d %>% filter(!subjid %in% drop$subjid)
 ##########################################
 # Define indicators of stunting at each time point
 ##########################################
-
 # define age windows
-d = d %>% 
-  mutate(agecat = case_when(
-    agedays==1 ~ "Birth",
-    agedays>1 & agedays<=3*30.4167 ~ "0-3 months",
-    agedays>3*30.4167 & agedays<=6*30.4167 ~ "3-6 months",
-    agedays>6*30.4167 & agedays<=9*30.4167 ~ "6-9 months",
-    agedays>9*30.4167 & agedays<=12*30.4167 ~ "9-12 months",
-    agedays>12*30.4167 & agedays<=15*30.4167 ~ "12-15 months",
-    agedays>15*30.4167 & agedays<=18*30.4167 ~"15-18 months",
-    agedays>18*30.4167 & agedays<=21*30.4167 ~ "18-21 months",
-    agedays>21*30.4167& agedays<=24*30.4167 ~ "21-24 months",
-    TRUE ~ ""
-  )) %>%
-  mutate(agecat=factor(agecat,levels=c("Birth","0-3 months","3-6 months","6-9 months",
-                                       "9-12 months","12-15 months","15-18 months",
-                                       "18-21 months","21-24 months")))
-
-# check age categories
-d %>%
-  group_by(agecat) %>%
-  summarise(n=sum(!is.na(agedays)),
-            min=min(agedays/30.4167),
-            mean=mean(agedays/30.4167),
-            max=max(agedays/30.4167))
-
-
-d <- d %>% mutate(agem=round(agedays/30.4167))
-
+d <- calc.ci.agecat(d, range = 3)
 
 rec.prev <- d %>%
 
