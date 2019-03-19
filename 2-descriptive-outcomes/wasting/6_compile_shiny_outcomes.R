f981c9e0e16c65542f7d5ade74f6638340794c97
Andrew Mertens <andrew.mertens@PROD.GHAP.IO>
2019-03-19 16:26:53 +0000

    mak ir per 1000

--- 2-descriptive-outcomes/wasting/6_compile_shiny_outcomes.R
+++ 2-descriptive-outcomes/wasting/6_compile_shiny_outcomes.R
@@ -11,15 +11,15 @@ stunt <- shiny_desc_data
 load("co_desc_data.Rdata")
 
 
-#Convert incidence rate to per 1000 days
-d$est[grepl("Incidence rate", d$measure)] <- d$est[grepl("Incidence rate", d$measure)] * 1000
-d$lb[grepl("Incidence rate", d$measure)] <- d$lb[grepl("Incidence rate", d$measure)] * 1000
-d$ub[grepl("Incidence rate", d$measure)] <- d$ub[grepl("Incidence rate", d$measure)] * 1000
-
-
 d <- bind_rows(stunt, wast, co_desc_data)
 d$agecat <- factor(d$agecat, levels=unique(d$agecat))
 d$region[d$region=="Asia" & !is.na(d$region)] <- "South Asia"
 d$region <- factor(d$region, levels=c("Overall","Africa","Latin America", "South Asia"))
+
+
+#Convert incidence rate to per 1000 days
+d$est[grepl("Incidence rate", d$measure)] <- d$est[grepl("Incidence rate", d$measure)] * 1000
+d$lb[grepl("Incidence rate", d$measure)] <- d$lb[grepl("Incidence rate", d$measure)] * 1000
+d$ub[grepl("Incidence rate", d$measure)] <- d$ub[grepl("Incidence rate", d$measure)] * 1000
 
 save(d, file=paste0(here(),"/results/desc_data_cleaned.Rdata"))
