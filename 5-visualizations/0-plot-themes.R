cb621388e4067d9774cb9b5c655034a419c8bdef
Andrew Mertens <amertens@berkeley.edu>
2019-03-25 09:40:22 -0700

    plot updates

--- 5-visualizations/0-plot-themes.R
+++ 5-visualizations/0-plot-themes.R
@@ -59,6 +59,8 @@ ki_desc_plot <- function(d, Disease, Measure, Birth, Severe, Age_range,
   df$agecat <- gsub(" months", "", df$agecat)
   df$agecat <- factor(df$agecat, levels=unique(df$agecat))
   
+  print(df)
+  
   p <- ggplot(df,aes(y=est,x=agecat)) +
     geom_errorbar(aes(color=region, ymin=lb, ymax=ub), width = 0) +
     geom_point(aes(fill=region, color=region), size = 2) +
