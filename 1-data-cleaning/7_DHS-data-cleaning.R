703f46740853b8e7cb0a8525ec8f0c26f10509fa
Wendy Jilek <wmjilek@berkeley.edu>
2019-04-09 17:27:17 -0700

    updated DHS cleaning script

--- 1-data-cleaning/7_DHS-data-cleaning.R
+++ 1-data-cleaning/7_DHS-data-cleaning.R
@@ -16,7 +16,7 @@ df <- read_rds(paste0(data_dir, "dhs-combined/", "dhs_data_combined.rds"))
 # Drop unnecessary variables and rename selected variables
 #-------------------------------------------
 #extra variables beginning with m or shhw are results of the stata lookfor function and
-#can be ignored
+#can be ignored4
 d <- df %>% rename(country = v000,
                    weight = v005,
                    cluster_no = v001,
@@ -24,6 +24,82 @@ d <- df %>% rename(country = v000,
                    stratification = v023) %>%
   select("caseid", "country", "weight", "dataset", "cluster_no", "psu", "stratification", grep("hw", colnames(df))) %>%
   select(-contains("sh"))
+
+d <- as.data.table(test)
+
+#recode country
+d <- d %>%
+  mutate(cntry_name=case_when(country=="AF7"~ "Afghanistan",
+                              country=="AO7"~ "Angola",
+                              country=="BD6"~ "Bangladesh",
+                              country=="BF6"~ "Burkina Faso",
+                              country=="BJ6"~ "Benin",
+                              country=="BO6"~ "Bolivia",
+                              country=="BR3"~ "Brazil",
+                              country=="BU7"~ "Burundi",
+                              country=="CD6"~ "Congo Democratic Republic",
+                              country=="CF3"~ "Central African Republic",
+                              country=="CG6"~ "Congo",
+                              country=="CI6"~ "Cote d'Ivoire",
+                              country=="CM6"~ "Cameroon",
+                              country=="CO7"~ "Colombia",
+                              country=="DR6"~ "Dominican Republic",
+                              country=="EC"~ "Ecuador",
+                              country=="ES"~ "El Salvador",
+                              country=="ET7"~ "Ethiopia",
+                              country=="GA6"~ "Gabon",
+                              country=="GH6"~ "Ghana",
+                              country=="GM6"~ "Gambia",
+                              country=="GN6"~ "Guinea",
+                              country=="GU6"~ "Guatemala",
+                              country=="GY5"~ "Guyana",
+                              country=="HN6"~ "Honduras",
+                              country=="HT7"~ "Haiti",
+                              country=="IA6"~ "India",
+                              country=="ID6"~ "Indonesia",
+                              country=="KE6"~ "Kenya",
+                              country=="KH6"~ "Cambodia",
+                              country=="KM6"~ "Comoros",
+                              country=="LB6"~ "Liberia",
+                              country=="LK"~ "Sri Lanka",
+                              country=="LS6"~ "Lesotho",
+                              country=="MD5"~ "Madagascar",
+                              country=="ML6"~ "Mali",
+                              country=="MM7"~ "Myanmar",
+                              country=="MV7"~ "Maldives",
+                              country=="MW7"~ "Malawi",
+                              country=="MX"~ "Mexico",
+                              country=="MZ6"~ "Mozambique",
+                              country=="NC4"~ "Nicaragua",
+                              country=="NG6"~ "Nigeria",
+                              country=="NI6"~ "Niger",
+                              country=="NM6"~ "Namibia",
+                              country=="NP7"~ "Nepal",
+                              country=="OS"~ "Nigeria (Ondo State)",
+                              country=="PE6"~ "Peru",
+                              country=="PH7"~ "Philippines",
+                              country=="PK7"~ "Pakistan",
+                              country=="PY2"~ "Paraguay",
+                              country=="RW6"~ "Rwanda",
+                              country=="SD"~ "Sudan",
+                              country=="SL6"~ "Sierra Leone",
+                              country=="SN7"~ "Senegal",
+                              country=="ST5"~ "Sao Tome and Princpe",
+                              country=="SZ5"~ "Swaziland",
+                              country=="TD6"~ "Chad",
+                              country=="TG6"~ "Togo",
+                              country=="TH"~ "Thailand",
+                              country=="TL7"~ "Timor-Leste",
+                              country=="TT"~ "Trinidad and Tobago",
+                              country=="TZ7"~ "Tanzania",
+                              country=="UG7"~ "Uganda",
+                              country=="VNT"~ "Vermont",
+                              country=="ZA7"~ "South Africa",
+                              country=="ZM6"~ "Zambia", 
+                              country=="ZW7"~ "Zimbabwe"))
+d$country <- NULL
+names(d)[names(dt)=="cntry_name"] <- "country"
+
 
 #check for duplicates
 table(duplicated(d))
@@ -43,7 +119,9 @@ d_haz_wide = d %>% select(caseid, country, dataset, weight, cluster_no, psu, str
 
 d_waz_wide = d %>% select(caseid, country, dataset, weight, cluster_no, psu, stratification,  year_vars, age_vars, waz_vars)
 
-d_whz_wide = d %>% select(caseid, country, dataset, weight, cluster_no, psu, stratification, year_vars, age_vars, whz_vars) #68 countries
+d_whz_wide = d %>% select(caseid, country, dataset, weight, cluster_no, psu, stratification, year_vars, age_vars, whz_vars) 
+
+#68 countries
 
 #-------------------------------------------
 # Reshape from wide to long 
@@ -145,7 +223,7 @@ d_whz_long <- d_whz_long %>% filter(agem <= 24)
 
 # drop rows with missing values 
 d_haz_long = d_haz_long %>% filter(!is.na(agem) & !is.na(zscore)) #51 countries!
-d_waz_long = d_waz_long %>% filter(!is.na(agem) & !is.na(zscore))
+d_waz_long = d_waz_long %>% filter(!is.na(agem) & !is.na(zscore)) 
 d_whz_long = d_whz_long %>% filter(!is.na(agem) & !is.na(zscore))
 
 
