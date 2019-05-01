bebb3595efcb8accffa84d9693a9965b4f0374b6
Andrew Mertens <amertens@berkeley.edu>
2019-05-01 12:30:58 -0700

    fix merge issue in CONSORT data

--- 1-data-cleaning/12_create_CONSORT_dataset.R
+++ 1-data-cleaning/12_create_CONSORT_dataset.R
@@ -65,12 +65,16 @@ unique(incl_long$Country)
 unique(incl_long$country)
 incl_long <- incl_long %>% subset(., select = -c(Country))
 
+#remove grant identifiers from studyid
+ns$Study_ID <- gsub("^k.*?-" , "", ns$Study_ID)
+
 #Merge datasets
 dim(ns)
 dim(incl_long)
 d <- left_join(ns, incl_long, by=c("Study_ID", "country"))
 dim(d)
 
+table(is.na(d$included_anthropometry))
 
 head(d)
 table(is.na(d$nchild))
@@ -84,42 +88,42 @@ unique(d$Study_ID)
 d$measurefreq <- NA
 
 d$measurefreq[d$Study_ID %in% c(
-  "ki0047075b-MAL-ED",   
-  "ki1000108-CMC-V-BCS-2002",              
-  "ki1000108-IRC",               
-  "ki1000109-EE",           
-  "ki1000109-ResPak",  
-  "ki1017093b-PROVIDE",  
-  "ki1066203-TanzaniaChild2",           
-  "ki1101329-Keneba",  
-  "ki1112895-Guatemala BSC",       
-  "ki1113344-GMS-Nepal",             
-  "ki1114097-CONTENT"
+  "MAL-ED",   
+  "CMC-V-BCS-2002",              
+  "IRC",               
+  "EE",           
+  "ResPak",  
+  "PROVIDE",  
+  "TanzaniaChild2",           
+  "Keneba",  
+  "Guatemala BSC",       
+  "GMS-Nepal",             
+  "CONTENT"
 )] <- "monthly"
 
 d$measurefreq[d$Study_ID %in% c(
-  "ki1112895-iLiNS-Zinc",  
-  "kiGH5241-JiVitA-3",          
-  "kiGH5241-JiVitA-4", 
-  "ki1148112-LCNI-5",          
-  "ki1017093-NIH-Birth",
-  "ki1017093c-NIH-Crypto",   
-  "ki1119695-PROBIT",         
-  "ki1000304b-SAS-CompFeed",   
-  "ki1000304b-SAS-FoodSuppl",   
-  "ki1126311-ZVITAMBO",   
-  "ki1114097-CMIN",                 
-  "ki1135781-COHORTS"
+  "iLiNS-Zinc",  
+  "JiVitA-3",          
+  "JiVitA-4", 
+  "LCNI-5",          
+  "NIH-Birth",
+  "NIH-Crypto",   
+  "PROBIT",         
+  "SAS-CompFeed",   
+  "SAS-FoodSuppl",   
+  "ZVITAMBO",   
+  "CMIN",                 
+  "COHORTS"
 )] <- "quarterly"
 
 
 #Mark COHORTS and CMIN cohorts with different measurement frequency than quarterly
-d$measurefreq[d$Study_ID=="ki1114097-CMIN" & d$country=="BANGLADESH"] <- "monthly"
-d$measurefreq[d$Study_ID=="ki1114097-CMIN" & d$country=="PERU"] <- "monthly"
+d$measurefreq[d$Study_ID=="CMIN" & d$country=="BANGLADESH"] <- "monthly"
+d$measurefreq[d$Study_ID=="CMIN" & d$country=="PERU"] <- "monthly"
 
 
-d$measurefreq[(d$Study_ID=="ki1135781-COHORTS" & d$country=="BRAZIL")] <- NA #Drop because yearly but not an RCT
-d$measurefreq[(d$Study_ID=="ki1135781-COHORTS" & d$country=="SOUTH AFRICA")] <- NA #Drop because yearly but not an RCT
+d$measurefreq[(d$Study_ID=="COHORTS" & d$country=="BRAZIL")] <- NA #Drop because yearly but not an RCT
+d$measurefreq[(d$Study_ID=="COHORTS" & d$country=="SOUTH AFRICA")] <- NA #Drop because yearly but not an RCT
 
 
 table(d$measurefreq)
@@ -130,6 +134,9 @@ d$included_monthly <- ifelse(d$measurefreq == "monthly", 1, 0)
 table(d$included_monthly)
 
 #NOTE: Need to make sure the country and studynames match so that all measurefreq get marked
+
+#Drop cross-sectional and non-qc rows
+d <- d %>% filter(included_qc==1)
 
 #Save dataset
 saveRDS(d, paste0(here::here(),"/results/HBGDki_CONSORT_inclusion_Ns.rds"))
