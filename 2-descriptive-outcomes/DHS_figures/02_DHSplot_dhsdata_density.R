2041ffdf882d43f67d76e4ae68bc3055dfb8313a
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-06-11 23:47:20 +0000

    consolidate dhs density figure scripts

---
+++ 2-descriptive-outcomes/DHS_figures/02_DHSplot_dhsdata_density.R
@@ -0,0 +1,103 @@
+##########################################
+# ki longitudinal manuscripts
+
+# This script reads in clean DHS z-score data and estimates kernel density of z-scores. 
+# Data is prepared and saved to produce a figure that compares DHS estimates to those from
+# the KI cohorts in the following scripts:
+#   - fig-DHS-plots-laz.R
+#   - fig-DHS-plots-scores.R
+
+# This script includes KI cohorts with measurement frequency at least monthly
+
+# Inputs:
+#   0-config.R : configuration file
+#   clean-DHS-haz.rds : clean DHS LAZ data
+#   clean-DHS-waz.rds : clean DHS WAZ data
+#   clean-DHS-whz.rds : clean DHS WHZ data
+#   ki.density.fits.monthly.rds : KI kernel densities of Z-scores for monthly cohorts
+#   ki.zscore.medians.monthly.rds : median values for monthly KI cohorts
+#   dhs.quantiles.rds : median values for DHS data
+
+# Outputs:
+#   figdata-fig_dhs_ki_zscores_density.RDS
+##########################################
+
+#---------------------------------------
+# source configuration file
+#---------------------------------------
+rm(list=ls())
+source(paste0(here::here(), "/0-config.R"))
+
+# set up for parallel computing
+# configure for a laptop (use only 3 cores)
+registerDoParallel(cores = 3)
+
+#---------------------------------------
+# load cleaned DHS anthro data
+# created by 7_DHS-data-cleaning.R
+#---------------------------------------
+dhaz <- readRDS(file = (here::here("data", "clean-DHS-haz.rds")))
+dwaz <- readRDS(file = (here::here("data", "clean-DHS-waz.rds")))
+dwhz <- readRDS(file = (here::here("data", "clean-DHS-whz.rds")))
+
+#---------------------------------------
+# combine all of the DHS data
+# into a single dataframe
+#---------------------------------------
+dhsz <- bind_rows(dhaz, dwaz, dwhz) %>%
+  mutate(
+    measure = factor(measure, levels = c("HAZ", "WAZ", "WHZ"), labels = c("LAZ", "WAZ", "WHZ"))
+  )
+
+#---------------------------------------
+# compute weights per instructions from
+# DHS
+#---------------------------------------
+dhsz <- dhsz %>%
+  mutate(wgt = weight / 1000000)
+
+
+#---------------------------------------
+# estimate DHS z-score densities
+# by region and overall
+#---------------------------------------
+dhsallden <- foreach(zmeas = levels(dhsz$measure), .combine = rbind, .packages = "dplyr") %:%
+  foreach(rgn = c("Africa", "South Asia", "Latin America"), .combine = rbind) %dopar% {
+    di <- dhsz %>% filter(measure == zmeas & region == rgn)
+    deni <- density(di$zscore)
+    denid <- data.frame(x = deni$x, y = deni$y, measure = zmeas, region = rgn)
+    denid
+  }
+dhsallden_pool <- foreach(zmeas = levels(dhsz$measure), .combine = rbind, .packages = "dplyr") %dopar% {
+  di <- dhsz %>% filter(measure == zmeas)
+  deni <- density(di$zscore)
+  denid <- data.frame(x = deni$x, y = deni$y, measure = zmeas, region = "Overall")
+  denid
+}
+dhsallden <- bind_rows(dhsallden, dhsallden_pool) %>%
+  mutate(region = factor(region, levels = c("Overall", "Africa", "South Asia", "Latin America")))
+
+#---------------------------------------
+# estimate DHS z-score densities
+# by region and overall
+# subset to countries that overlap KI cohorts
+#---------------------------------------
+dhssubden <- foreach(zmeas = levels(dhsz$measure), .combine = rbind) %:%
+  foreach(rgn = c("Africa", "South Asia", "Latin America"), .combine = rbind) %dopar% {
+    di <- dhsz %>% filter(measure == zmeas & region == rgn & inghap == 1)
+    deni <- density(di$zscore)
+    denid <- data.frame(x = deni$x, y = deni$y, measure = zmeas, region = rgn)
+    denid
+  }
+dhssubden_pool <- foreach(zmeas = levels(dhsz$measure), .combine = rbind) %dopar% {
+  di <- dhsz %>% filter(measure == zmeas & inghap == 1)
+  deni <- density(di$zscore)
+  denid <- data.frame(x = deni$x, y = deni$y, measure = zmeas, region = "Overall")
+  denid
+}
+dhssubden <- bind_rows(dhssubden, dhssubden_pool) %>%
+  mutate(region = factor(region, levels = c("Overall", "Africa", "South Asia", "Latin America")))
+
+#Save densities
+saveRDS(dhsallden, file = paste0(here(),"/results/dhs.density.all-countries.rds"))
+saveRDS(dhssubden, file = paste0(here(),"/results/dhs.density.ki-countries.rds"))
---
+++ 2-descriptive-outcomes/DHS_figures/03_DHSplot_density_medians.R
@@ -0,0 +1,41 @@
+# Find DHS medians
+dhs_quantiles <- readRDS(paste0(here::here(),"/results/dhs.quantiles.rds"))
+dhs_medians = dhs_quantiles %>% filter(quantile == 50)
+dhs_medians$region = recode_factor(dhs_medians$region, 
+                                   OVERALL = "Overall", 
+                                   AFRO = "Africa", 
+                                   PAHO = "Latin America", 
+                                   SEARO = "South Asia")
+
+dhs_medians =  dhs_medians %>% mutate(
+  measure = factor(measure, levels = c("haz", "waz", "whz"), labels = c("LAZ", "WAZ", "WHZ"))
+)
+
+names(dhs_medians)[2] = "median"
+dhs_medians = select(dhs_medians, c("region", "measure", "median"))
+dhs_medians$dsource = "DHS, ki countries"
+
+# Find KI medians, combine KI and DHS medians into a single table
+ki_medians_monthly = readRDS(paste0("results/ki.zscore.medians.monthly.rds"))
+ki_medians_quarterly = readRDS(paste0("results/ki.zscore.medians.quarterly.rds"))
+
+calc_medians = function(ki_medians){
+  ki_medians$dsource = "ki cohorts"
+  ki_medians$region = recode_factor(ki_medians$region, 
+                                    OVERALL = "Overall", 
+                                    AFRO = "Africa", 
+                                    PAHO = "Latin America", 
+                                    SEARO = "South Asia")
+  
+  ki_medians =  ki_medians %>% mutate(
+    measure = factor(measure, levels = c("haz", "waz", "whz"), labels = c("LAZ", "WAZ", "WHZ"))
+  )
+  medians = rbind(ki_medians, dhs_medians)
+  return (ki_medians)
+}
+
+medians_quarterly = calc_medians(ki_medians_quarterly) %>% rbind(., dhs_medians)
+medians_monthly = calc_medians(ki_medians_monthly) %>% rbind(., dhs_medians)
+
+saveRDS(medians_quarterly, file = paste0(here(),"/results/dhs.ki.zscore.medians.quarterly.rds"))
+saveRDS(medians_monthly, file = paste0(here(),"/results/dhs.ki.zscore.medians.monthly.rds"))
