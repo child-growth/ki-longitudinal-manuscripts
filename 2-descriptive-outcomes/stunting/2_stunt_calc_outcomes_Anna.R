4e17f5fa1808de1ab433b85085360ba9a1943ab0
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-07-09 07:44:06 +0000

    edits to stunt calc outcomes

--- 2-descriptive-outcomes/stunting/2_stunt_calc_outcomes_Anna.R
+++ 2-descriptive-outcomes/stunting/2_stunt_calc_outcomes_Anna.R
@@ -32,7 +32,7 @@ source(paste0(here::here(), "/0-config.R"))
 source(paste0(here::here(), "/0-project-functions/0_descriptive_epi_shared_functions.R"))
 source(paste0(here::here(), "/0-project-functions/0_descriptive_epi_stunt_functions.R"))
 
-load("U:/ucb-superlearner/data/stunting_data.RData")
+d <- readRDS(paste0(ghapdata_dir, "stunting_data.rds"))
 
 head(d)
 d <- d %>% subset(., select = -c(tr))
@@ -91,7 +91,7 @@ agelst6_birthstrat = list(
 )
 
 calc_outcomes = function(data, calc_method, output_file_suffix){
-  dprev <<- calc.prev.agecat(d)
+  dprev <<- calc.prev.agecat(data)
   dmon <<- calc.monthly.agecat(data)
   d3 <<- calc.ci.agecat(data, range = 3, birth="yes")
   d6 <<- calc.ci.agecat(data, range = 6, birth="yes")
@@ -128,31 +128,25 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
   ######################################################################
   # Mean HAZ
   ######################################################################
-  calc_mean_haz = function(data){
-    haz.data <- summary.haz.age.sex(data, method = calc_method)
-    haz.region <-  data  %>% group_by(region) %>% do(summary.haz.age.sex(., method = calc_method)$haz.res)
-    haz.cohort <-
-      haz.data$haz.cohort %>% 
-      subset(., select = c(cohort, region, agecat, sex, nmeas, meanhaz, ci.lb,  ci.ub)) %>%
-      rename(est = meanhaz,  lb = ci.lb,  ub = ci.ub)
-    
-    haz <- bind_rows(
-      data.frame(cohort = "pooled", region = "Overall", haz.data$haz.res),
-      data.frame(cohort = "pooled", haz.region),
-      haz.cohort
-    )
-    return(haz)
-  }
-  
   #----------------------------------------
   # mean haz
   #----------------------------------------
-  haz = calc_mean_haz(dprev)
+  haz.data <- summary.haz(dprev, method = calc_method)
+  haz.region <- dprev %>% group_by(region) %>% do(summary.haz(., method = calc_method)$haz.res)
+  haz.cohort <-
+    haz.data$haz.cohort %>% subset(., select = c(cohort, region, agecat, nmeas,  meanhaz,  ci.lb,  ci.ub)) %>%
+    rename(est = meanhaz,  lb = ci.lb,  ub = ci.ub)
+  
+  haz <- bind_rows(
+    data.frame(cohort = "pooled", region = "Overall", haz.data$haz.res),
+    data.frame(cohort = "pooled", haz.region),
+    haz.cohort
+  )
   
   #----------------------------------------
   # mean haz for growth velocity age categories
   #----------------------------------------
-  d_vel = d %>% 
+  d_vel = data %>% 
     mutate(agecat=ifelse(agedays<3*30.4167,"0-3",
                          ifelse(agedays>=3*30.4167 & agedays<6*30.4167,"3-6",
                                 ifelse(agedays>=6*30.4167 & agedays<9*30.4167,"6-9",
@@ -164,14 +158,26 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
     mutate(agecat=factor(agecat,levels=c("0-3","3-6","6-9","9-12",
                                          "12-15","15-18","18-21","21-24"))) 
   
-  haz.vel = calc_mean_haz(d_vel)
+  haz.data.vel <- summary.haz.age.sex(d_vel, method = calc_method)
+  haz.region.vel <-  d_vel  %>% group_by(region) %>% do(summary.haz.age.sex(., method = calc_method)$haz.res)
+  haz.cohort.vel <-
+    haz.data.vel$haz.cohort %>% 
+    subset(., select = c(cohort, region, agecat, sex, nmeas,  meanhaz, 
+                         ci.lb,  ci.ub)) %>%
+    rename(est = meanhaz,  lb = ci.lb,  ub = ci.ub)
+  
+  haz.vel <- bind_rows(
+    data.frame(cohort = "pooled", region = "Overall", haz.data.vel$haz.res),
+    data.frame(cohort = "pooled", haz.region.vel),
+    haz.cohort.vel
+  )
   
   saveRDS(haz.vel, file = paste0(here(), "/results/meanlaz_velocity", output_file_suffix, ".RDS"))
   
   #----------------------------------------
   # monthly mean haz
   #----------------------------------------
-  dmon <- calc.monthly.agecat(d)
+  dmon <- calc.monthly.agecat(data)
   monthly.haz.data <- summary.haz(dmon, method = calc_method)
   monthly.haz.region <-  dmon  %>% group_by(region) %>% do(summary.haz(., method = calc_method)$haz.res)
   monthly.haz.cohort <-
@@ -212,15 +218,14 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
   # combine data
   quantiles <- bind_rows(quantile_d, quantile_d_overall,quantile_d_cohort)
   
-  saveRDS(quantiles,
-          file = paste0(here(),"/results/quantile_data_stunting", output_file_suffix, ".RDS"))
+  saveRDS(quantiles,file = paste0(here(),"/results/quantile_data_stunting", output_file_suffix, ".RDS"))
   
   ######################################################################
   # Incidence proportion
   ######################################################################
-  calc_ip = function(d, age_list, severe){
-    ip.data <- summary.stunt.incprop(d, agelist = age_list, severe.stunted = severe, method = calc_method)
-    ip.region <- d %>% group_by(region) %>% do(summary.stunt.incprop(., agelist = age_list, severe.stunted = severe, method = calc_method)$ip.res)
+  calc_ip = function(datatable, age_list, severe){
+    ip.data <- summary.stunt.incprop(datatable, agelist = age_list, severe.stunted = severe, method = calc_method)
+    ip.region <- datatable %>% group_by(region) %>% do(summary.stunt.incprop(., agelist = age_list, severe.stunted = severe, method = calc_method)$ip.res)
     ip.cohort <-
       ip.data$ip.cohort %>% 
       subset(., select = c(cohort, region, agecat, nchild,  yi,  ci.lb,  ci.ub)) %>%
@@ -266,9 +271,9 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
   # Cumulative incidence
   ######################################################################
   
-  calc_ci = function(d, age_list, birth_strat, severe){
-    ci.data <- summary.ci(d, birthstrat = birth_strat, agelist = age_list, severe.stunted = severe, method = calc_method)
-    ci.region <- d %>% group_by(region) %>% do(summary.ci(., agelist = age_list,  birthstrat = birth_strat, severe.stunted = severe, method = calc_method)$ci.res)
+  calc_ci = function(datatable, age_list, birth_strat, severe){
+    ci.data <- summary.ci(datatable, birthstrat = birth_strat, agelist = age_list, severe.stunted = severe, method = calc_method)
+    ci.region <- datatable %>% group_by(region) %>% do(summary.ci(., agelist = age_list,  birthstrat = birth_strat, severe.stunted = severe, method = calc_method)$ci.res)
     ci.cohort <-
       ci.data$ci.cohort %>% subset(., select = c(cohort, region, agecat, nchild,  yi,  ci.lb,  ci.ub)) %>%
       rename(est = yi,  lb = ci.lb,  ub = ci.ub, nmeas=nchild)
@@ -280,6 +285,14 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
     )
     return(cuminc)
   }
+  
+  
+  
+  cuminc3.birthstrat <- bind_rows(
+    data.frame(cohort = "pooled", region = "Overall", ci.data3.birthstrat$ci.res),
+    data.frame(cohort = "pooled", ci.region3.birthstrat),
+    ci.cohort3.birthstrat
+  )
     
   #----------------------------------------
   # Cumulative Incidence  - 3 month intervals
@@ -307,7 +320,7 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
   # Cumulative Incidence  - 6 month intervals
   # severe
   #----------------------------------------
-  sev.cuminc6 <- calc_ci(d6, agelst6, birth_strat = FALSE, severe = TRUE)
+  sev.cuminc6.1 <- calc_ci(d6, agelst6, birth_strat = FALSE, severe = TRUE)
   
   shiny_desc_data <- bind_rows(
     data.frame(disease = "Stunting", age_range="3 months",   birth="yes", severe="no", measure= "Prevalence", prev),
@@ -326,23 +339,23 @@ calc_outcomes = function(data, calc_method, output_file_suffix){
     data.frame(disease = "Stunting", age_range="6 months",   birth="yes", severe="yes", measure= "Incidence_proportion",  sev.ip6)
   )
   
+  assert_that(names(table(shiny_desc_data$method.used)) == calc_method)
+  
   shiny_desc_data <- shiny_desc_data %>% subset(., select = -c(se, nmeas.f,  ptest.f))
   
-  unique(shiny_desc_data$agecat)
   shiny_desc_data$agecat <- as.factor(shiny_desc_data$agecat)
   
-  unique(shiny_desc_data$region)
   shiny_desc_data$region <- factor(shiny_desc_data$region, levels=c("Overall", "Africa", "Latin America", "Asia"))
     
   return(shiny_desc_data)
 }
 
-stunt_outcomes = calc_outcomes(d, "REML", "")
+stunt_outcomes = calc_outcomes(data = d, calc_method = "REML", output_file_suffix = "")
 saveRDS(stunt_outcomes, file = paste0(res_dir,"shiny_desc_data_stunting_objects.RDS"))
 
-stunt_outcomes_monthly = calc_outcomes(monthly_d, "REML", "_monthly")
-saveRDS(stunt_outcomes_monthly, file =  paste0(res_dir, "shiny_desc_data_stunting_objects_monthly24.RDS"))
+stunt_outcomes_monthly = calc_outcomes(data = monthly_d, calc_method = "REML", output_file_suffix = "_monthly")
+saveRDS(stunt_outcomes_monthly, file =  paste0(res_dir, "shiny_desc_data_stunting_objects_monthly.RDS"))
 
-stunt_outcomes_fe = calc_outcomes(d, "FE", "_fe")
+stunt_outcomes_fe = calc_outcomes(data = d, calc_method = "FE", output_file_suffix = "_fe")
 saveRDS(stunt_outcomes_fe, file = paste0(res_dir,"shiny_desc_data_stunting_objects_fe.RDS"))
 
