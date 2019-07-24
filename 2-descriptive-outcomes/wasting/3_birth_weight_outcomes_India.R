099b3a66be7af8124cc7ca65a08f53278433501c
Andrew Mertens <andrew.mertens@PROD.GHAP.IO>
2019-07-24 03:02:02 +0000

    lbw strat stats plot

--- 2-descriptive-outcomes/wasting/3_birth_weight_outcomes_India.R
+++ 2-descriptive-outcomes/wasting/3_birth_weight_outcomes_India.R
@@ -7,7 +7,33 @@ source(paste0(here::here(), "/0-config.R"))
 source(paste0(here::here(),"/0-project-functions/0_descriptive_epi_wast_functions.R"))
 source(paste0(here::here(),"/0-project-functions/0_descriptive_epi_co_functions.R"))
 
+#Co-occurrence data
+co <- readRDS(paste0(ghapdata_dir, "co_occurrence_data.rds"))
 
+#Subset to monthly
+co <- co %>% filter(measurefreq == "monthly") %>% filter(agedays < 24*30.4167) %>%
+  filter(region=="South Asia") %>%
+  subset(., select = c(studyid, region, country, subjid, agedays, whz, haz, sex))
+
+#Mark children born or enrolled wasted
+co <- co %>% group_by(studyid, subjid) %>% arrange(studyid, subjid, agedays) %>%
+  mutate(age_enrol = first(agedays),
+         first_whz = first(whz)) %>%
+  filter(age_enrol == 1) %>%
+  mutate(born_wast = 1 * (first_whz < (-2)))
+
+#calculate birthweight
+co$birthwt <- who_zscore2wtkg(co$age_enrol, co$first_whz, sex = co$sex) * 1000
+summary(co$birthwt)
+
+#Categorize low birthweight
+co$lbw <- NA
+co$lbw[co$birthwt<2500] <- 1
+co$lbw[co$birthwt>=2500] <- 0
+table(co$lbw)
+
+
+#Wasting data
 load(paste0(ghapdata_dir, "Wasting_inc_data.RData"))
 
 #Subset to monthly
@@ -17,12 +43,13 @@ d <- d_noBW %>% filter(measurefreq == "monthly") %>% filter(agedays < 24*30.4167
 
 #Mark children born or enrolled wasted
 d <- d %>% group_by(studyid, subjid) %>% arrange(studyid, subjid, agedays) %>%
-  mutate(age_enrol = first(agedays)) %>%
+  mutate(age_enrol = first(agedays),
+         first_whz = first(whz)) %>%
   filter(age_enrol == 1) %>%
-  mutate(born_wast = 1 * (first(whz) < (-2)))
+  mutate(born_wast = 1 * (first_whz < (-2)))
 
 #calculate birthweight
-d$birthwt <- who_zscore2wtkg(d$age_enrol, d$whz, sex = d$sex) * 1000
+d$birthwt <- who_zscore2wtkg(d$age_enrol, d$first_whz, sex = d$sex) * 1000
 summary(d$birthwt)
 
 #Categorize low birthweight
@@ -74,8 +101,6 @@ p <- ggplot(data=plotdf, aes(x=agedays, y=fit, group=lbw, color=lbw,  fill=lbw))
     legend.background = element_blank(),
     legend.box.background = element_rect(colour = "black"),
     legend.text=element_text(size=rel(1)))    
-p
-
 
 ggsave(p, file=paste0(here(),"/figures/India/wasting/birthweight_strat_growth_curve_India.png"), width=7, height=5)
 
@@ -87,53 +112,53 @@ d$whz[d$wasting_episode=="Born Wasted"] <- NA
 
 
 d <- calc.ci.agecat(d, range=6)
+co <- calc.ci.agecat(co, range=6)
 
 #Drop first 6 months
 d <- d %>% filter(agecat!="0-6 months")
+co <- co %>% filter(agecat!="0-6 months")
 
 d$agecat <- "6-24 months"
+co$agecat <- "6-24 months"
 d$agecat <- factor(d$agecat)
+co$agecat <- factor(co$agecat)
 
 #mean whz
-whz.res <- d %>% group_by(born_wast) %>% do(summary.whz(.)$whz.res)
+whz.res <- d %>% group_by(lbw) %>% do(summary.whz(.)$whz.res)
 whz.res
 
-#Cumulative inc
-ci.res <- d %>% group_by(born_wast) %>% do(summary.ci(., agelist = list("6-24 months"))$ci.res)
-ci.res
-
-sev.ci.res <- d %>% group_by(born_wast) %>% do(summary.ci(., agelist = list("6-24 months"), severe.wasted = T)$ci.res)
-sev.ci.res
+# #Cumulative inc
+# ci.res <- d %>% group_by(lbw) %>% do(summary.incprop(.)$ci.res)
+# ci.res
+# 
+# sev.ci.res <- d %>% group_by(lbw) %>% do(summary.incprop(., severe.wasted = T)$ci.res)
+# sev.ci.res
 
 #Incidence rate
-ir.res <- d %>% group_by(born_wast) %>% do(summary.ir(., agelist = list("6-24 months"))$ir.res)
+ir.res <- d %>% group_by(lbw) %>% do(summary.ir(.)$ir.res)
 ir.res$est <- ir.res$est * 1000
 ir.res$lb <- ir.res$lb * 1000
 ir.res$ub <- ir.res$ub * 1000
 
 
 
-#Duration
-# dur.res <- d %>% group_by(born_wast) %>% do(summary.dur(., agelist = list("6-24 months"))$dur.res)
-# dur.res
-
 #Persistant wasting
-perswast.res <- d %>% group_by(born_wast) %>% do(summary.perswast(., agelist = list("6-24 months"))$pers.res)
+perswast.res <- d %>% group_by(lbw) %>% do(summary.perswast(.)$pers.res)
 perswast.res
 
 
 
 #co-occurrent wasting and stunting
-co.res <- d %>% group_by(born_wast) %>% do(summary.prev.co(.)$prev.res)
+co.res <- co %>% group_by(lbw) %>% do(summary.prev.co(.)$prev.res)
 co.res
 
 
 res <- data.frame(
-  measure= rep(c("Mean WLZ","Wasting cumulative incidence", "Severe wasting cumulative incidence", "Wasting incidence rate", "Persistent wasting", "Co-occurrent wasting and stunting"), each=2),
-  rbind(whz.res,ci.res, sev.ci.res, ir.res, perswast.res, co.res)
+  measure= rep(c("Mean WLZ", "Wasting incidence rate", "Persistent wasting", "Co-occurrent wasting and stunting"), each=2),
+  rbind(whz.res, ir.res, perswast.res, co.res)
 )
 res
 
-saveRDS(res, file = paste0(here(),"/results/bw_longterm_res_India.rds"))
+saveRDS(res, file = paste0(here(),"/results/lbw_longterm_res_India.rds"))
 
 
