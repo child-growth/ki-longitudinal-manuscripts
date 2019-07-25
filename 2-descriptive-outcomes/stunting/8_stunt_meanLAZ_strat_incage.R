68469f2bde6fa035edf709886fafecb92630c1a3
Anna Nguyen <anna-nguyen@berkeley.edu>
2019-07-25 23:52:45 +0000

    laz strat incage

--- 2-descriptive-outcomes/stunting/8_stunt_meanLAZ_strat_incage.R
+++ 2-descriptive-outcomes/stunting/8_stunt_meanLAZ_strat_incage.R
@@ -40,25 +40,33 @@ d_st = create_stunting_age_indicators(data = d)
 #----------------------------------------
 age_list = list("Birth", "3 months", "6 months", "Never")
 
-dmon_incage <- lapply(age_list, function(x) 
-  calc.monthly.agecat(d = d_st %>% filter(stunt_inc_age == x)))
+# dmon_incage <- lapply(age_list, function(x) 
+#   calc.monthly.agecat(d = d_st %>% filter(stunt_inc_age == x)))
 
-### ANNA: please update this code chunk to run over the list dmon_incage instead of dmon
-monthly.haz.data   <-  summary.haz(dmon, method = calc_method)
-monthly.haz.region <-  dmon  %>% group_by(region) %>%
-                                 do(summary.haz(., method = calc_method)$haz.res)
-monthly.haz.cohort <-  monthly.haz.data$haz.cohort %>% 
-  subset(., select = c(cohort, region, agecat, nmeas,  meanhaz,  ci.lb,  ci.ub)) %>%
-  rename(est = meanhaz,  lb = ci.lb,  ub = ci.ub)
+meanlaz = function(age){ 
+  dmon = calc.monthly.agecat(d = d_st %>% filter(stunt_inc_age == age))
+  #dmon = calc.monthly.agecat(d = d_st %>% filter(stunt_inc_age == "3 months"))
+  
+  monthly.haz.data   <-  summary.haz(data = dmon)
+  monthly.haz.region <-  dmon  %>% group_by(region) %>% do(summary.haz(.)$haz.res)
+  monthly.haz.cohort <-  monthly.haz.data$haz.cohort %>% 
+    subset(., select = c(cohort, region, agecat, nmeas,  meanhaz,  ci.lb,  ci.ub)) %>%
+    rename(est = meanhaz,  lb = ci.lb,  ub = ci.ub)
+  
+  monthly.haz <- bind_rows(
+    data.frame(cohort = "pooled", region = "Overall", monthly.haz.data$haz.res),
+    data.frame(cohort = "pooled", monthly.haz.region),
+    monthly.haz.cohort
+  )
+  
+  return (monthly.haz)
+}
 
-monthly.haz <- bind_rows(
-  data.frame(cohort = "pooled", region = "Overall", monthly.haz.data$haz.res),
-  data.frame(cohort = "pooled", monthly.haz.region),
-  monthly.haz.cohort
-)
+meanlaz_age_incage = lapply(age_list, meanlaz)
+
 
 #######################################################################
 # save data
 #######################################################################
-# saveRDS(ANNA_ADD_NAME, file = paste0(res_dir, "meanlaz_age_incage.RDS"))
+# saveRDS(meanlaz_age_incage, file = paste0(res_dir, "meanlaz_age_incage.RDS"))
  
