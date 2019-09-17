2843f49a5f97f8251389f564b47979cd6768ba7b
PROD\andrew.mertens <amertens@berkeley.edu>
2019-09-17 04:01:28 +0000

    seasonality rg analysis

--- 5-visualizations/wasting/manuscript_figure_grids.R
+++ 5-visualizations/wasting/manuscript_figure_grids.R
@@ -6,6 +6,36 @@ source(paste0(here::here(), "/0-project-functions/0_clean_study_data_functions.R
 
 require(cowplot)
 
+
+
+#Figure 3 alt
+load(here("/figures/plot objects/season_diff_plot.Rdata"))
+load(here("/figures/plot objects/season_plots.Rdata"))
+plot_list <- readRDS(paste0(here(),"/results/rain_seasonality_plot_objects.rds"))
+
+#Left plot
+left_plot <- plot_grid(
+  plot_list[[10]], plot_list[[1]], plot_list[[11]],
+  plot_list[[2]], plot_list[[12]], 
+  plot_list[[3]], plot_list[[13]], plot_list[[4]],
+  plot_list[[14]], plot_list[[5]], plot_list[[15]],
+  plot_list[[6]], plot_list[[16]], plot_list[[7]],
+  plot_list[[17]], plot_list[[8]], 
+  plot_list[[18]], plot_list[[9]], 
+  labels = rep("", 18), ncol = 2, align = 'v', axis = 'l')
+
+#Right plot
+right_plot <- plot_grid(p2, p3, p4, pdiff, labels = c("B","C","D","E"), ncol = 1, align = 'v', axis = 'l')
+
+
+fig3 <- plot_grid(left_plot, right_plot, labels = c("A",""), ncol = 2, align = 'h', axis = 'l', rel_heights = c(1, 1))
+
+ggsave(fig3, file=paste0(here(),"/figures/manuscript figure composites/wasting/fig3_alt.png"), width=14, height=14)
+
+
+
+
+
 #Figure 2
 load(here("/figures/plot objects/fig2_plot_objects.Rdata"))
 
@@ -18,14 +48,14 @@ ggsave(fig2, file=paste0(here(),"/figures/manuscript figure composites/wasting/f
 
 
 #Figure 3
-load(here("/figures/plot objects/season_diff_plot.Rdata"))
-load(here("/figures/plot objects/season_plots.Rdata"))
-
-TwobyTwoplot <- plot_grid(p1, p2, p3, p4, labels = "AUTO", ncol = 2, align = 'v', axis = 'l')
-
-fig3 <- plot_grid(TwobyTwoplot, pdiff, labels = c("","E"), ncol = 1, align = 'h', axis = 'l', rel_heights = c(2, 1))
-
-ggsave(fig3, file=paste0(here(),"/figures/manuscript figure composites/wasting/fig3.png"), width=14, height=14)
+# load(here("/figures/plot objects/season_diff_plot.Rdata"))
+# load(here("/figures/plot objects/season_plots.Rdata"))
+# 
+# TwobyTwoplot <- plot_grid(p1, p2, p3, p4, labels = "AUTO", ncol = 2, align = 'v', axis = 'l')
+# 
+# fig3 <- plot_grid(TwobyTwoplot, pdiff, labels = c("","E"), ncol = 1, align = 'h', axis = 'l', rel_heights = c(2, 1))
+# 
+# ggsave(fig3, file=paste0(here(),"/figures/manuscript figure composites/wasting/fig3.png"), width=14, height=14)
 
 #Figure 4
 load(here("/figures/plot objects/rec_plot_object.Rdata"))
