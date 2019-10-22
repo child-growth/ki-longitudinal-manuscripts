7c468c5af9446d770b06e42fe0b327b97c48f919
Andrew Mertens <andrew.mertens@PROD.GHAP.IO>
2019-10-22 19:04:43 +0000

    add in number of children

--- 1-data-cleaning/QA checks/create_agecheck_dataset.R
+++ 1-data-cleaning/QA checks/create_agecheck_dataset.R
@@ -4,6 +4,9 @@ source(paste0(here::here(), "/0-config.R"))
 
 d <- readRDS(ki_manuscript_dataset_path)
 
-d <- d %>% subset(., select = c(studyid, country, agedays))
+#Calc number of children in each study
+d <- d %>% group_by(studyid, country) %>% mutate(n_children = length(unique(subjid)))
+
+d <- d %>% subset(., select = c(studyid, country, n_children, agedays))
 
 saveRDS(d, file=here("results/study_age_check_data.RDS"))
