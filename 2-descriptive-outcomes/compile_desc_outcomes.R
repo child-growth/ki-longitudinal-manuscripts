1147c201f08bfc79742e4e1877b328f9611ed959
PROD\andrew.mertens <amertens@berkeley.edu>
2019-12-16 13:43:30 +0000

    co-occurrence no kenaba bw

--- 2-descriptive-outcomes/compile_desc_outcomes.R
+++ 2-descriptive-outcomes/compile_desc_outcomes.R
@@ -20,6 +20,7 @@ stunt_noProbit = readRDS(paste0(res_dir,"shiny_desc_data_stunting_objects_no_pro
 stunt_noProbit_fe <- readRDS(paste0(res_dir,"shiny_desc_data_stunting_objects_fe_no_probit.RDS")) %>% mutate(analysis = "No Probit - Fixed effects")
 
 wast_noKenabaBirth =  readRDS(paste0(res_dir,"wasting_desc_data_no_Kenaba_bw.RDS")) %>% mutate(analysis = "No Kenaba birth")
+co_noKenabaBirth <- readRDS(paste0(res_dir,"co_desc_data_no_Kenaba.RDS")) %>% mutate(analysis = "No Kenaba birth")
 
 stunt_noKenaba_monthly24 = readRDS(paste0(res_dir,"shiny_desc_data_stunting_objects_monthly24_no_Kenaba_birth.RDS")) %>% mutate(analysis = "No Kenaba - monthly 0-24 m", measure=paste0(measure," - monthly cohorts"))
 stunt_noKenaba = readRDS(paste0(res_dir,"shiny_desc_data_stunting_objects_no_Kenaba_birth.RDS")) %>% mutate(analysis = "No Kenaba")
@@ -35,7 +36,7 @@ d <- bind_rows(stunt, wast, co_desc_data,
                stunt_fe, wast_fe, co_fe,
                stunt_monthly24, 
                stunt_noProbit, stunt_noProbit_fe, 
-               wast_noKenabaBirth,
+               wast_noKenabaBirth, co_noKenabaBirth,
                stunt_noKenaba, stunt_noKenaba_fe, stunt_noKenaba_monthly24)
 
 
