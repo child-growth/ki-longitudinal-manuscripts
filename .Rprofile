18043f6443645435b960e3fafa3ac91e8db58053
Stephanie Djajadi <sdjajadi@berkeley.edu>
2020-01-22 18:44:16 -0800

    run stunting visualization bash script

---
+++ .Rprofile
@@ -0,0 +1,2 @@
+kiPath <- c("/data/KI/R/x86_64-pc-linux-gnu-library/3.6/", .libPaths())
+.libPaths(kiPath)
--- 0-config.R
+++ 0-config.R
@@ -5,9 +5,6 @@
 # source base functions
 # load libraries
 #-------------------------------------
-kiPath <- c("/data/KI/R/x86_64-pc-linux-gnu-library/3.6/" , .libPaths())
-.libPaths(kiPath)
-
 
 library(here)
 library(tidyverse)
