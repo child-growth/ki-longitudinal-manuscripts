dc07a4ac1a56a74e9f194056a0d95234be479e3a
Stephanie Djajadi <sdjajadi@berkeley.edu>
2021-03-14 19:26:56 -0700

    add snow flurries to pers wasting fe

--- 5-visualizations/wasting/fig-pers-wasting-FE.R
+++ 5-visualizations/wasting/fig-pers-wasting-FE.R
@@ -1,11 +1,9 @@
-
-
 rm(list=ls())
 source(paste0(here::here(), "/0-config.R"))
 
 #Load data
 load(paste0(here(),"/results/persistent_wasting024_FE.Rdata"))
-d <- perswast024 %>% filter(cohort=="pooled", is.na(country))
+d <- perswast024 %>% filter(is.na(country))
 
 d$nmeas.f <- clean_nmeans(d$nmeas)
 d$nstudy.f <- gsub("N=","",d$nstudy.f)
@@ -13,28 +11,55 @@ d$nmeas.f <- gsub("N=","",d$nmeas.f)
 d$nstudy.f <- gsub(" studies","",d$nstudy.f)
 d$nmeas.f <- gsub(" children","",d$nmeas.f)
 
-
-xlabel="Region"
-ylabel="Point prevalence (95% CI"
-
 d$region[d$region=="Asia"] <- "South Asia"
 d$region <- factor(d$region , levels=c("Overall","Africa","Latin America","South Asia"))
 
+# scale cohort-specific estimates
+d = d %>% mutate(
+  est = ifelse(cohort!="pooled", est*100, est),
+  lb = ifelse(cohort!="pooled", lb*100, lb),
+  ub = ifelse(cohort!="pooled", ub*100, ub)
+)
+
+d <- d %>% mutate(ispooled = as.factor(ifelse(cohort=="pooled", "yes", "no")))
+
+xlabel="Region"
+ylabel="Point prevalence (95% CI)"
+
+
 p <- ggplot(d,aes(y=est,x=region)) +
-  geom_errorbar(aes(color=region, ymin=lb, ymax=ub), width = 0) +
-  geom_point(aes(fill=region, color=region), size = 2) +
-  geom_text(aes(x = region, y = est, label = round(est)), hjust = 1.5) +
-  scale_color_manual(values=tableau11) +
+  
+  # pooled 
+  geom_point(aes(size=2, fill=region, color=region),
+             size = 2, stroke = 0,
+             data = d %>% filter(ispooled == "yes")) +
+  
+  geom_errorbar(aes(color=region, ymin=lb, ymax=ub), width = 0,
+                data = d %>% filter(ispooled == "yes")) +
+  
+  geom_text(data=d %>% filter(ispooled == "yes"),
+            aes(x = region, y = est, label = round(est)),
+            hjust = 1.5,
+            vjust = 0.5) +
+  
+  # cohort-stratified
+  geom_point(color = "#878787", fill = "#878787", size = 1.5,
+             data = d %>% filter(ispooled == "no"),
+             position = position_jitter(width = 0.15), alpha = 0.25) +
+  
+  scale_color_manual(values=tableau11, drop=TRUE, guide = FALSE) +
+  scale_size_manual(values = c(2, 1.5), guide = FALSE) +
+  scale_fill_manual(values=tableau11, guide = FALSE) +
+
   xlab(xlabel)+
   ylab(ylabel) +
   scale_y_continuous(breaks = scales::pretty_breaks(n = 10)) +
   theme(strip.text = element_text(size=15, margin = margin(t = 0))) +
   
-  theme(axis.text.x = element_text(margin = 
-                                     margin(t = 0, r = 0, b = 0, l = 0),
+  theme(axis.text.x = element_text(margin = margin(t=0, r=0, b=0, l=0),
                                    size = 12)) +
   theme(axis.title.y = element_text(size = 12)) 
+
 p
 
 ggsave(p, file=here("/figures/wasting/pooled_pers024_FE.png"), width=8, height=5)
-
