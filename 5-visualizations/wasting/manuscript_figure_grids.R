5dea2d506d8483a4aec8c1c6222ec13abcc61ec5
Esther Jung <esthjung@berkeley.edu>
2021-10-20 09:50:45 -0700

    Standardized wasting plots for Nature

--- 5-visualizations/wasting/manuscript_figure_grids.R
+++ 5-visualizations/wasting/manuscript_figure_grids.R
@@ -20,7 +20,7 @@ ci_plot <- plot_list[[3]]$`plot` + theme(legend.position = c(.07,.8))
 fig2 <- plot_grid(whz_spline, prev_p, ci_plot, labels = "auto", ncol = 1, align = 'v', axis = 'l')
 
 #ggsave(fig2, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig2.png"), width=14, height=14)
-ggsave(fig2, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig2.png"), width=14, height=10)
+ggsave(fig2, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig2.png"), width=14, height=12)
 
 #N's for figure caption
 plot_list[[1]]$`data`  %>% group_by(region) %>% summarize(min(N), max(N))
@@ -45,7 +45,7 @@ load(paste0(BV_dir,"/figures/plot-objects/stunting_rain_seasonality_plot_paramet
 #Add birth diff inset plot
 #seasonbirth_plot <- seasonbirth_plot_list[[2]] +ylab("Birth WLZ") + inset(ggplotGrob(seasonbirth_diff), xmin = 80, xmax = 300, ymin = -0.6, ymax = 0) 
 seasonbirth_plot <- seasonbirth_plot_list[[2]]  + inset(ggplotGrob(seasonbirth_diff), xmin = 2.5, xmax = 11, ymin = 300, ymax = 540) 
-ggsave(seasonbirth_plot, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig3_inset.png"), width=7, height=5)
+ggsave(seasonbirth_plot, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig3_inset.png"), width=7, height=10)
 
 
 plot_list <- readRDS(paste0(BV_dir,"/figures/plot-objects/rain_seasonality_plot_objects.rds"))
@@ -114,7 +114,7 @@ fig4_a <- plot_grid(ind_traj, rec_violin_plot, labels = c("a","c"), ncol = 2, al
 fig4_c <- plot_grid(birthstrat_curve, birthstrat_stats_plot, labels = c("e","f"), ncol = 2, align = 'v', axis = 'l')
 
 fig4 <- plot_grid(fig4_a, inc_plot, rec_plot[[1]], fig4_c, labels = c("","b","d",""), ncol = 1, align = 'h', axis = 'l', rel_heights = c(1,1,1, 1))
-ggsave(fig4, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig4.png"), width=14, height=14)
+ggsave(fig4, file=paste0(BV_dir,"/figures/manuscript-figure-composites/wasting/fig4.png"), width=16, height=14)
 
 
 #Figure 5
