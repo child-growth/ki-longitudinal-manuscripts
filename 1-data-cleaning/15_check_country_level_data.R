f09babd0e397e07bc274f7883313b978c1f99d59
andrew.mertens <andrew.mertens3@gmail.com>
2022-05-06 20:27:21 -0700

    update

---
+++ 1-data-cleaning/15_check_country_level_data.R
@@ -0,0 +1,9 @@
+
+
+
+
+rm(list=ls())
+source(paste0(here::here(), "/0-config.R"))
+df <- readRDS(file="/data/KI/UCB-SuperLearner/Manuscript analysis data/ki-country-metrics.rds")
+
+head(df)
--- 1-data-cleaning/4_create_master_dataset.R
+++ 1-data-cleaning/4_create_master_dataset.R
@@ -7,6 +7,9 @@ source(paste0(here::here(), "/0-config.R"))
 #load longform anthropometry and mortality data
 Zscores <- readRDS(included_studies_path)
 
+#load country metrics
+country_metrics <- readRDS(file=here("data/country metrics/combined_country_metrics.RDS"))
+country_metrics <- country_metrics %>% rename(brthyr = year) %>% mutate(country=str_to_upper(country))
 
 # Check how many at-birth measurements have
 # length < 45cm and therefore no Z-scores
@@ -27,6 +30,51 @@ Zscores <- Zscores %>%
 #mark regions 
 Zscores <- mark_region(Zscores)
 table(Zscores$region)
+
+#fill in missing birthyr
+table(Zscores$studyid, is.na(Zscores$brthyr))
+Zscores$brthyr[Zscores$studyid=="NIH-Birth"] <- 2008
+Zscores$brthyr[Zscores$studyid=="NIH-Crypto"] <- 2014
+Zscores$brthyr[Zscores$studyid=="PROBIT"] <- 1996
+Zscores$brthyr[Zscores$studyid=="PROVIDE"] <- 2011
+Zscores <- Zscores %>% group_by(studyid, country) %>% mutate(brthyr=ifelse(is.na(brthyr),floor(mean(brthyr, na.rm=T)),brthyr))
+table(Zscores$studyid, is.na(Zscores$brthyr))
+unique(Zscores$brthyr)
+
+
+
+#merge country metrics
+Zscores <- left_join(Zscores, country_metrics, by = c("country","brthyr"))
+# head(df)
+# table(is.na(df$gii))
+# 
+# table(df$studyid, is.na(df$brthyr))
+# 
+# table(df$country, is.na(df$gdp))
+# table(df$country, is.na(df$gii))
+# table(country_metrics$country)
+# 
+# #some missing birthyears
+# df[is.na(df$gdp) & df$country=="PAKISTAN",]
+# 
+# #explore variation in country-level metrics
+# ggplot(df, aes(x=gdp)) + geom_histogram()
+# ggplot(df, aes(x=gdp)) + geom_density() + facet_wrap(~country)
+# df[df$gdp==max(df$gdp, na.rm=T),]
+# 
+# ggplot(df, aes(x=gdi)) + geom_histogram()
+# ggplot(df, aes(x=gii)) + geom_histogram()
+# ggplot(df, aes(x=chi)) + geom_histogram()
+# ggplot(df, aes(x=gini)) + geom_histogram()
+# ggplot(df, aes(x=he)) + geom_histogram()
+# ggplot(df, aes(x=pov)) + geom_histogram()
+# 
+# 
+# #put histograms in google doc after imputing birthyear and subsetting to 1-row per child
+# #stunting incidence
+# table(df$studyid, (df$gini>50))
+
+
 
 
 #load covariate dataset (one row per child)
@@ -86,6 +134,8 @@ table(d$cohortid)
 d$exclude_desc <- ifelse(d$agedays > 25 * 30.4167, 1, 0)
 table(d$exclude_desc)
 
+
+
 # Save dataset
 saveRDS(d, ki_manuscript_dataset_path)
 write.csv(d, "/data/KI/UCB-SuperLearner/Manuscript analysis data/ki-manuscript-dataset.csv")
@@ -110,4 +160,23 @@ start_year$max_yr[start_year$studyid=="NIH-Crypto"] <- 2017
 start_year$max_yr[start_year$studyid=="Burkina Faso Zn"] <- 2012
 
 saveRDS(start_year, paste0(here::here(),"/data/study_start_years.rds"))
+
+
+#save birth years
+brthyrs <- d %>% group_by(studyid, country) %>% distinct(brthyr)
+saveRDS(brthyrs, paste0(here::here(),"/data/study_birth_years.rds"))
+
+
+#creat dataset of unique children and country metrics
+colnames(d)
+
+head(d)
+
+df <- d %>%  group_by(studyid, country, subjid) %>% slice(1) %>% 
+             select(studyid, subjid, country, brthyr, region, gdp,gdi,gii,chi,gini,he,pov)
+head(df)
+
+#classify study decade
+df <- left_join(df, start_year, by = c("studyid", "country"))
+saveRDS(df, file="/data/KI/UCB-SuperLearner/Manuscript analysis data/ki-country-metrics.rds")
 
