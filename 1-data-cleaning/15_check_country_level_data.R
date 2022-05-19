c0e81215f7481374f01196598104e99116c9cedd
Andrew Mertens <amertens@berkeley.edu>
2022-05-19 10:26:15 -0700

    update

--- 1-data-cleaning/15_check_country_level_data.R
+++ 1-data-cleaning/15_check_country_level_data.R
@@ -4,7 +4,7 @@
 
 rm(list=ls())
 source(paste0(here::here(), "/0-config.R"))
-df <- readRDS(file="/data/KI/UCB-SuperLearner/Manuscript analysis data/ki-country-metrics.rds")
+df <- readRDS(file=here("/data/KI/UCB-SuperLearner/Manuscript analysis data/combined_country_metrics.RDS"))
 
 head(df)
 
