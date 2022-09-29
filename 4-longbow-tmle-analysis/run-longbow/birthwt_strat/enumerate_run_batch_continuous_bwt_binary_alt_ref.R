4cc33621d8132ae7840981670313625cb4969f38
andrew.mertens <andrew.mertens3@gmail.com>
2022-09-29 11:16:22 -0700

    rerun with new ref

---
+++ 4-longbow-tmle-analysis/run-longbow/birthwt_strat/enumerate_run_batch_continuous_bwt_binary_alt_ref.R
@@ -0,0 +1,170 @@
+
+rm(list=ls())
+try(.libPaths( c( "/data/KI/R/x86_64-pc-linux-gnu-library/4.0/" , .libPaths() ) ))
+
+source(paste0(here::here(), "/0-config.R"))
+try(.libPaths( "~/rlibs" ))
+library(data.table)
+library(longbowtools)
+library(progress)
+library(longbowRiskFactors)
+
+
+# 1. enumerate analysis
+setwd(here("4-longbow-tmle-analysis","run-longbow","primary-analysis"))
+inputs <- "inputs_template.json"
+default_params <- jsonlite::fromJSON(inputs)
+
+#Set to continious
+default_params$script_params$count_Y <- FALSE
+
+#load analyses
+load(here("4-longbow-tmle-analysis","analysis specification","adjusted_continuous.rdata"))
+
+#Drop growth velocity
+dim(analyses)
+analyses <- analyses %>% filter(Y=="haz" | Y=="whz", !(A %in% c("perdiar24","predfeed3","predfeed6","predfeed36","exclfeed3",
+                                                                 "exclfeed6","exclfeed36", "anywast06","pers_wast","brthmon","month","gagebrth","birthwt","birthlen","enstunt","enwast")))
+dim(analyses)
+
+analyses$strata
+
+unique(analyses$A)
+analyses2 <- analyses
+
+#drop birth size as RF, and drop birth size from covariate set
+for(i in 1:nrow(analyses)){
+  if(!is.null(analyses$W[[i]])){
+    analyses$W[[i]] <- analyses$W[[i]][!(analyses$W[[i]] %in% c("gagebrth","birthwt","birthlen","enstunt","enwast"))]
+  }
+  analyses$strata[[i]] <- c("agecat","studyid","country","birthwt")
+}
+
+
+load("/data/KI/UCB-SuperLearner/Manuscript analysis data/wast_meanZ_rf.Rdata")
+d <- d %>% filter(!is.na(birthwt), agecat=="24 months") %>% droplevels()
+table(d$agecat)
+
+risk_var_to_binary <- function(d){
+  
+  for(i in 1:ncol(d)){
+    if(is.factor(d[,i])){d[,i] <- fct_rev(d[,i])}
+  }
+  
+
+  
+  d <- d %>%
+    mutate(
+      mhtcm  =factor(mhtcm , levels=c("<150 cm", ">=150 cm")),
+      fhtcm   =factor(fhtcm  , levels=c(  "<162 cm",  ">=162 cm")),
+      mwtkg   =factor(mwtkg  , levels=c("<45 kg" , ">=45 kg")),
+      mbmi    =factor(mbmi  , levels=c("<20 kg/m²", ">=20 kg/m²")),
+      vagbrth     =factor(vagbrth   , levels=c("1","0")),
+      hdlvry     =factor(hdlvry   , levels=c("1","0")),
+      trth2o      =factor(trth2o    , levels=c("0","1")),
+      cleanck      =factor(cleanck    , levels=c("0","1")),
+      impfloor      =factor(impfloor    , levels=c("0","1")),
+      nchldlt5      =factor(nchldlt5    , levels=c("2+","1")),
+      earlybf      =factor(earlybf    , levels=c("0","1")),
+      gagebrth  = factor(case_when(
+        gagebrth =="Full or late term" ~ "Preterm",
+        gagebrth !="Full or late term" ~ "Full term"), levels=c("Preterm","Full term")),
+      mage = factor(case_when(
+        mage=="<20" ~ "<20",
+        mage!="<20" ~ ">20"), levels=c("<20",">20")),
+      fage = factor(case_when(
+        fage=="<30" ~ "<30",
+        fage!="<30" ~ ">30"), levels=c("<30",">30")),
+      nrooms = factor(case_when(
+        nrooms=="1" ~ "1",
+        nrooms!="1" ~ "2+"), levels=c("1","2+")),
+      nhh = factor(case_when(
+        nhh %in% c("3 or less", "4-5") ~ "<=5",
+        nhh %in% c("6-7","8+") ~ ">5"), levels=c(">5","<=5")),
+      parity = factor(case_when(
+        parity!="1" ~ "Secondborn+",
+        parity=="1" ~ "Firstborn"
+        ), levels=c("Secondborn+","Firstborn")),
+      meducyrs = factor(case_when(
+        meducyrs=="Low" ~ "Low",
+        meducyrs!="Low" ~ "Med/High"), levels=c("Low","Med/High")),
+      feducyrs = factor(case_when(
+        feducyrs=="Low" ~ "Low",
+        feducyrs!="Low" ~ "Med/High"), levels=c("Low","Med/High")),
+      hfoodsec = factor(case_when(
+        hfoodsec!="Food Secure" ~ "Food Insecure",
+        hfoodsec=="Food Secure" ~ "Food Secure"
+        ), levels=c("Food Insecure","Food Secure")),
+      hhwealth_quart = factor(case_when(
+        hhwealth_quart %in% c("WealthQ1", "WealthQ2") ~ "LowWealth",
+        hhwealth_quart %in% c("WealthQ4", "WealthQ3") ~ "HighWealth"      
+      ), levels=c("LowWealth","HighWealth")))
+  
+  d <- as.data.frame(d)
+
+  
+  
+  return(d)
+}
+
+
+d <- risk_var_to_binary(d)
+
+
+glimpse(d)
+for(i in 10:41){
+  cat(colnames(d)[i],"\n")
+  print(levels(d[,i]))
+}
+
+
+save(d, file="/data/KI/UCB-SuperLearner/Manuscript analysis data/wast_meanZ_rf_bwt_strat_bin_alt_ref.Rdata")
+
+load("/data/KI/UCB-SuperLearner/Manuscript analysis data/st_meanZ_rf.Rdata")
+d <- d %>% filter(!is.na(birthwt), agecat=="24 months")  %>% droplevels()
+
+d <- risk_var_to_binary(d)
+
+
+# d$nrooms <- factor(d$nrooms, levels=c("1","2","3","4+"))
+# d$mage <- factor(d$mage, levels=c("<20","[20-30)",">=30"))
+# d$fage <- factor(d$fage, levels=c("<30","[30-35)",">=35"))
+# d$meducyrs <- factor(d$meducyrs, levels=c("Low", "Medium" ,"High" ))
+# d$feducyrs <- factor(d$feducyrs, levels=c("Low", "Medium" ,"High" ))
+# d$birthlen <- factor(d$birthlen, levels=c("<48 cm", "[48-50) cm", ">=50 cm"))
+# d$gagebrth <- factor(d$gagebrth, levels=c("Preterm", "Early term", "Full or late term")) 
+
+
+
+save(d, file="/data/KI/UCB-SuperLearner/Manuscript analysis data/st_meanZ_rf_bwt_strat_bin_alt_ref.Rdata")
+
+
+analyses <- analyses %>% filter(!(A %in% c("brthmon","month",  "enstunt",   "single",    "enwast","predfeed3",  "birthlen", "birthwt",   "predfeed6",     "predfeed36",    "exclfeed3","exclfeed6",     "exclfeed36","fhtcm_rf",  "anywast06",     "pers_wast",   
+                                           "predfeed3",     "predfeed6",     "predfeed36",    "exclfeed3",     "exclfeed6",     "exclfeed36" )))
+
+analyses$strata
+#specify analyses
+analyses$file <- gsub("rf.Rdata","rf_bwt_strat_bin_alt_ref.Rdata",analyses$file)
+
+#temp
+enumerated_analyses <- lapply(seq_len(nrow(analyses)), specify_longbow)
+
+
+run_ki_tmle(enumerated_analyses, results_folder="results_cont_bwt_strat_bin_alt_ref", overwrite = F, skip_failed = F)
+
+
+
+analyses2$file <- gsub("rf.Rdata","rf_bwt_strat_bin.Rdata",analyses2$file)
+for(i in 1:nrow(analyses2)){
+  analyses2$strata[[i]] <- c("agecat",  "studyid", "country")
+}
+
+
+enumerated_analyses2 <- lapply(seq_len(nrow(analyses2)), analyses_df=analyses2, specify_longbow)
+run_ki_tmle(enumerated_analyses2, results_folder="results_cont_full_bin_alt_ref", overwrite = F, skip_failed = F)
+
+
+#Exclude events without a certain number of strata
+
+#Exclude anything with less than 10 or less than 50 observations
+
--- 4-longbow-tmle-analysis/temp_bwt_data_checking.R
+++ 4-longbow-tmle-analysis/temp_bwt_data_checking.R
@@ -31,10 +31,49 @@ d2 <- d %>% mutate(birthwt="Unstratified")
 d <- bind_rows(d, d2)
 d <- d %>% filter(agecat=="24 months", !is.na(sex))
 d %>% group_by(studyid, country, birthwt,sex) %>% summarize(haz=mean(haz), N=n())
-d %>% group_by(studyid, country, birthwt,sex) %>% summarize(haz=mean(haz), N=n()) %>%
-  group_by(studyid, country, birthwt) %>% summarize(haz=last(haz)-first(haz), N=sum(N))
+df <- d %>% group_by(studyid, country, birthwt,sex) %>% mutate(N=n()) %>% filter(N>50) %>% droplevels() %>%
+                 summarize(haz=mean(haz), N=n()) %>%
+                 group_by(studyid, country, birthwt) %>% 
+                 summarize(haz=last(haz)-first(haz), N=sum(N)) %>% group_by(studyid, country) %>%
+                 mutate(Nstrat=n()) %>% filter(Nstrat==3)
+
+#Zivitambo and Jivita 3 and 4
+
+
+df <- d %>% filter(studyid == "CMC-V-BCS-2002", country == "INDIA", birthwt == "Normal or high birthweight", sex == "Female")
+t.test(df$haz)$"conf.int"[2]
+
+
+mean_ci <- function(d){
+  haz=mean(d$haz)
+  ci.lb=t.test(d$haz)$"conf.int"[1]
+  ci.ub=t.test(d$haz)$"conf.int"[2]
+  N=nrow(d)
+  res=data.frame(haz=haz, ci.lb=ci.lb, ci.ub=ci.ub, N=N)
+  return(res)
+}
+
+df <- d %>% group_by(studyid, country, birthwt, sex) %>% mutate(N=n()) %>% filter(N>50) %>% droplevels() %>%
+  do(mean_ci(.)) %>% mutate(cohort=paste0(studyid, " ", country), strat=paste0(birthwt, ", ", sex)) %>% group_by(studyid, country) %>%
+  mutate(Nstrat=n()) %>% filter(Nstrat==6)
+
+
+ggplot(df, aes(x=strat, y=haz)) +
+  geom_point() +
+  geom_linerange(aes(ymin=ci.lb, ymax=ci.ub)) +
+  facet_wrap(~cohort) +
+  coord_flip()
 
 
 d %>% group_by(studyid, country,sex) %>% summarize(haz=mean(haz), N=n()) %>%
   group_by(studyid, country) %>% summarize(haz=last(haz)-first(haz), N=sum(N)) %>% filter(N>50)
 
+
+d <- d %>% filter(studyid %in% c("JiVitA-3","JiVitA-4","ZVITAMBO"))
+df <- d %>% group_by(studyid, country) %>% mutate(Ntotal=n()) %>%
+  group_by(studyid, country, sex) %>% mutate(Nsex=n()) %>%
+  group_by(studyid, country, birthwt,sex) %>% 
+  summarize(haz=mean(haz), N=n(), Nsex=Nsex[1], Ntotal=Ntotal[1]) %>%
+  group_by(studyid, country, birthwt) %>% 
+  summarize(haz_diff=last(haz)-first(haz), Nfemale=first(Nsex), Nmale=last(Nsex), Ntotal=Ntotal[1])
+df
