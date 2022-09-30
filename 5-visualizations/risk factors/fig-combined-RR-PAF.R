580bc4ccdabb093fe002a8a123c524378a2c8544
andrew.mertens <andrew.mertens3@gmail.com>
2022-09-30 14:40:30 -0700

    rrerun

--- 5-visualizations/risk factors/fig-combined-RR-PAF.R
+++ 5-visualizations/risk factors/fig-combined-RR-PAF.R
@@ -29,7 +29,8 @@ main_color <- "#287D8EFF"
 
 
 # CIR
-CIR_raw <- readRDS(paste0(BV_dir,"/results/rf results/pooled_RR_results_alt_ref.rds")) %>% mutate(parameter="CIR") 
+#CIR_raw <- readRDS(paste0(BV_dir,"/results/rf results/pooled_RR_results_alt_ref.rds")) %>% mutate(parameter="CIR") 
+CIR_raw <- readRDS(paste0(BV_dir,"/results/rf results/pooled_RR_results.rds")) %>% mutate(parameter="CIR") 
 #Prev 
 #prev_raw <- readRDS(paste0(BV_dir,"/results/rf results/pooled_RR_results.rds")) %>% mutate(parameter="Prev") %>% filter(agecat=="24 months",intervention_variable!="perdiar24")
 #prev_raw <-NULL
