19889809ea54734ee316df66c1d23ff7d0d52351
andrew.mertens <andrew.mertens3@gmail.com>
2022-12-01 19:12:13 -0800

    rerun

--- 4-longbow-tmle-analysis/run-longbow/primary-analysis/enumerate_run_batch_primary_alt_ref.R
+++ 4-longbow-tmle-analysis/run-longbow/primary-analysis/enumerate_run_batch_primary_alt_ref.R
@@ -47,7 +47,8 @@ table(d$nrooms)
 # )
 
 levels(d$sga)
-d$nrooms <- factor(d$nrooms, levels=c("1","2","3","4+"))
+d$nhh <- factor(d$nhh, levels=c("8+","6-7","4-5","3 or less"))
+d$nrooms <- factor(d$nrooms, levels=rev(c("1","2","3","4+")))
 d$sga <- factor(d$sga, levels=c("Not SGA","SGA"))
 d$parity <- factor(d$parity, levels=c("2","1","3+"))
 d$mage <- factor(d$mage, levels=c("<20","[20-30)",">=30"))
@@ -70,7 +71,8 @@ for(i in 1:ncol(d)){
 #   nrooms = factor(case_when(nrooms=="1" ~ "1",   nrooms=="2"|nrooms=="3"|nrooms=="4+" ~ "2+"), levels = c("1", "2+"))
 # )
 
-d$nrooms <- factor(d$nrooms, levels=c("1","2","3","4+"))
+d$nhh <- factor(d$nhh, levels=c("8+","6-7","4-5","3 or less"))
+d$nrooms <- factor(d$nrooms, levels=rev(c("1","2","3","4+")))
 d$sga <- factor(d$sga, levels=c("Not SGA","SGA"))
 d$parity <- factor(d$parity, levels=c("2","1","3+"))
 d$mage <- factor(d$mage, levels=c("<20","[20-30)",">=30"))
@@ -88,11 +90,13 @@ for(i in 1:ncol(d)){
   if(is.factor(d[,i])){d[,i] <- fct_rev(d[,i])}
 }
 
-d <- d %>% mutate(
-  nhh = factor(case_when(nhh=="8+"|nhh=="6-7" ~ "6+",   nhh=="4-5"|nhh=="3 or less" ~ "5 or less"), levels = c("5 or less", "6+")),
-  nrooms = factor(case_when(nrooms=="1" ~ "1",   nrooms=="2"|nrooms=="3"|nrooms=="4+" ~ "2+"), levels = c("1", "2+"))
-)
+# d <- d %>% mutate(
+#   nhh = factor(case_when(nhh=="8+"|nhh=="6-7" ~ "6+",   nhh=="4-5"|nhh=="3 or less" ~ "5 or less"), levels = c("5 or less", "6+")),
+#   nrooms = factor(case_when(nrooms=="1" ~ "1",   nrooms=="2"|nrooms=="3"|nrooms=="4+" ~ "2+"), levels = c("1", "2+"))
+# )
 
+d$nhh <- factor(d$nhh, levels=c("8+","6-7","4-5","3 or less"))
+d$nrooms <- factor(d$nrooms, levels=rev(c("1","2","3","4+")))
 d$sga <- factor(d$sga, levels=c("Not SGA","SGA"))
 d$parity <- factor(d$parity, levels=c("2","1","3+"))
 d$mage <- factor(d$mage, levels=c("<20","[20-30)",">=30"))
@@ -114,7 +118,8 @@ for(i in 1:ncol(d)){
 #   nhh = factor(case_when(nhh=="8+"|nhh=="6-7" ~ "6+",   nhh=="4-5"|nhh=="3 or less" ~ "5 or less"), levels = c("5 or less", "6+")),
 #   nrooms = factor(case_when(nrooms=="1" ~ "1",   nrooms=="2"|nrooms=="3"|nrooms=="4+" ~ "2+"), levels = c("1", "2+"))
 # )
-d$nrooms <- factor(d$nrooms, levels=c("1","2","3","4+"))
+d$nhh <- factor(d$nhh, levels=c("8+","6-7","4-5","3 or less"))
+d$nrooms <- factor(d$nrooms, levels=rev(c("1","2","3","4+")))
 d$sga <- factor(d$sga, levels=c("Not SGA","SGA"))
 d$parity <- factor(d$parity, levels=c("2","1","3+"))
 d$mage <- factor(d$mage, levels=c("<20","[20-30)",">=30"))
